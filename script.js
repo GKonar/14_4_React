9da74ad1c03b3ff58e54777d6c1d6285e02623e8
Grzegorz Konarski <grzegorz.konarski86@gmail.com>
2018-04-11 11:55:54 +0200

    Change structure

--- script.js
+++ script.js
@@ -1,68 +1,94 @@
-var movies = [
+	var movies = [
 	{
 		id: 1,
 		title: 'Harry Potter',
 		desc: 'Film o czarodzieju',
-		img: 'https://i.ebayimg.com/images/g/Z54AAOSwnFZXV1Eh/s-l300.jpg'
+		image: 'https://i.ebayimg.com/images/g/Z54AAOSwnFZXV1Eh/s-l300.jpg'
 	},														//Tablica z obiekatmi
 	{
 		id: 2,
 		title: 'Król Lew',
 		desc: 'Film opowidający historię króla sawanny',
-		img: 'http://1.fwcdn.pl/po/68/78/6878/6927221.6.jpg'
+		image: 'http://1.fwcdn.pl/po/68/78/6878/6927221.6.jpg'
 	},
 	{
 		id: 3,
 		title: "Pan życia i śmierci",
 		desc: "Film o handlarzu bronią",
-		img: "http://www.placecage.com/g/155/300"
+		image: "http://www.placecage.com/g/155/300"
 	},
 	{
 		id: 4,
 		title: "Lot skazańców",
 		desc: "Film o locie skazańców :)",
-		img: "http://www.placecage.com/c/460/300"
+		image: "http://www.placecage.com/c/460/300"
 	},
 	{
 		id: 5,
 		title: "Bez twarzy",
 		desc: "Kolejny świetny film z Nickiem :)",
-		img: "http://www.placecage.com/c/200/300"
+		image: "http://www.placecage.com/c/200/300"
 	},
 ];
 
-var Movie = React.createClass({								// 1.
+
+
+
+//Title
+
+var MovieTitle = React.createClass({
+	propTypes:{
+		title: React.PropTypes.object.isRequired,
+	},
+
 	render: function(){
-	 return React.createElement('li', {key: movie.id},	
-			React.createElement('h2', {}, movie.title),
-			React.createElement('p', {}, movie.desc),
-			React.createElement('img', {src: movie.img}) 
-		)
-	} 
+		return(	React.createElement('h2', {}, this.props.movie.title) //funkcja render zawsze zwraca (return) ReactElement
+		);
+	}
 });
-/*
-var moviesElements = movies.map(function(movie) {  
- 	React.createClass({
- 		render: function(){
- 			return 
- 		}
- 	})										   	 
-}); 
-*/
-var moviesElements = 
-	React.createClass({
-		render: function(){
-			return movies.map(function(movie) {React.createElement('Movie', {}, movie),
-			}		
-		})	
-	}); 
+
+//Description
+
+var MovieDescription = React.createClass({
+	propTypes:{
+		desc: React.PropTypes.object.isRequired,
+	},
+
+	render: function(){
+		return ( React.createElement('p', {}, this.props.movie.desc)
+		);
+	}
+});
+
+//Poster 
+
+var MoviePoster = React.createClass({
+	propTypes: {
+		image: React.PropTypes.object.isRequired,
+	},
+
+	render: function(){
+		return (React.createElement('img', {src: this.props.movie.image})
+		);
+	}
+});
+
+//Movie List
+
+var MoviesList = React.createClass({
+	render: function(){
+		return (React.createElement('div', {},
+					React.createElement('h1', {}, 'Lista Filmów'),
+					React.createElement('ul', {}, 
+						React.createElement('li', {key: this.props.movie.id},
+							React.createElement(MovieTitle, this.props.movie.title),
+							React.createElement(MovieDescription, this.props.movie.desc),
+							React.createElement(MoviePoster, this.props.movie.image)
+						)		
+				));
+	}
+});
 
 
-var element = 
-	React.createElement('div', {}, 
-		React.createElement('h1', {}, 'Lista filmów'),
-		React.createElement('ul', {}, moviesElements) // umieszczenie powyższych elementów w liście
-	);
 
 
-ReactDOM.render(element, document.getElementById('app'));
